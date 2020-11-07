f9ec64170d99f4d3c41621e0d56c2a89a36f1a95
madasamynagarajan <madasamynagaraj@outlook.com>
2020-11-08 01:42:46 +0530

    init setup commit

---
+++ SMS.Model/ViewModel/AccountViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMS.Model.Model;
+using System.Data.OracleClient;
+using SMS.DAO;
+using SMS.UTILITY;
+using System.Data;
+namespace SMS.Model.ViewModel
+{
+    public class AccountViewModel : IDisposable
+    {
+        public LoginViewModel objLoginViewModel { get; set; }
+        public List<Notifications> liNotification { get; set; }
+        public static List<User> FetchUserDetails(UserDetails objUserDetails)
+        {
+            DataValue dv = new DataValue();
+            dv.Add("@USERNAME", objUserDetails.E_USER_ID, EnumCommand.DataType.Varchar);
+            dv.Add("@PASSWORD", objUserDetails.E_PASSWORD, EnumCommand.DataType.Varchar);
+            dv.Add("@ORGID", objUserDetails.E_COMPANY, EnumCommand.DataType.Int);
+            //var liUserDetails = (List<User>)OracleHelper.FetchData<User>(Oraparam, Common.Queries.CMS_SP_INSERT_USER_MANAGER, EnumCommand.DataSource.list).DataSource.Data;
+            //return liUserDetails;
+
+            var liUserDetails = (List<User>)SQLHelper.FetchData<User>(Common.Queries.SP_LOGIN_USER, EnumCommand.DataSource.list, dv).DataSource.Data;
+            return liUserDetails;
+
+
+        }
+
+        public static List<SubMenuItems> FetchUserSubMenu(UserDetails objUserDetails)
+        {
+            DataValue dv = new DataValue();
+            dv.Add("@ROLE_CODE", objUserDetails.E_ROLE_CODE, EnumCommand.DataType.Varchar);
+            dv.Add("@ORGID", objUserDetails.E_COMPANY, EnumCommand.DataType.Int);
+            var liSubUserMenu = (List<SubMenuItems>)SQLHelper.FetchData<SubMenuItems>(Common.Queries.SP_MENU_LIST, EnumCommand.DataSource.list, dv).DataSource.Data;
+            return liSubUserMenu;
+        }
+       
+        #region IDisposeMethod
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+        #endregion
+    }
+}
---
+++ SMS/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SMS.DAO;
+using SMS.UTILITY;
+using System.Web.Mvc;
+using System.Web.Security;
+using SMS.Model.ViewModel;
+using System.Data.OleDb;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net;
+using Newtonsoft.Json;
+using SMS.Model.Model;
+
+namespace SMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        public static string UserId = string.Empty;
+        public static string UserName = string.Empty;
+        public static string Companycode = string.Empty;
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            //RedirectToAction("Index","Administration");
+            return View();
+        }
+
+        public ActionResult Loginauth()
+        {
+
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult SignIn(Model.ViewModel.AccountViewModel collection)
+        {
+            string sUserRole = string.Empty;
+            string sLastLogin = string.Empty;
+
+            UserDetails objUserDetails = new UserDetails();
+            objUserDetails.E_USER_ID = collection.objLoginViewModel.Username;
+            objUserDetails.E_PASSWORD = collection.objLoginViewModel.Password;
+            //objUserDetails.E_COMPANY = collection.objLoginViewModel.Companycode;
+           // objUserDetails.E_COMPANY = ConfigurationManager.AppSettings["companycode"].ToString(); 
+            var LiUserCredential = AccountViewModel.FetchUserDetails(objUserDetails);
+            if (LiUserCredential != null && LiUserCredential.Count > 0)
+            {
+                Session[Common.SESSION_VARIABLES.USERNAME] = collection.objLoginViewModel.Username;
+                Session[Common.SESSION_VARIABLES.PASSWORD] = collection.objLoginViewModel.Password;
+                Session[Common.SESSION_VARIABLES.COMPANYCODE] = LiUserCredential[0].Companycode;
+                Session[Common.SESSION_VARIABLES.ROLE_CODE] = LiUserCredential[0].ROLE_CODE;
+
+                objUserDetails.E_COMPANY = Session[Common.SESSION_VARIABLES.COMPANYCODE].ToString();
+                objUserDetails.E_ROLE_CODE = Session[Common.SESSION_VARIABLES.ROLE_CODE].ToString();
+                List<SubMenuItems> LiUserMenu = AccountViewModel.FetchUserSubMenu(objUserDetails);
+                if (LiUserMenu.Count > 0)
+                {
+                    Session[Common.SESSION_VARIABLES.MENU_ITEMS] = LiUserMenu;
+                }
+
+            }
+            else
+            {
+                TempData["ErrorMessage"] = Common.Message.InvalidUserOrNotExist;
+                return RedirectToAction("Login", "Account");
+            }
+            //return RedirectToAction(objUserDetails.E_ROLE_CODE, "DashBoard");
+            return RedirectToAction("ADMIN", "DashBoard");
+
+
+
+
+        }
+
+        public ActionResult SignOut()
+        {
+            
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+
+    }
+}
