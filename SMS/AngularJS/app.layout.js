418748803e7756a5229113a31e7899b72639b031
vijayever <vijaykani.e@outlook.com>
2020-11-09 18:32:08 +0530

    SMS Meanu header style and logo colapsible

---
+++ SMS/AngularJS/app.layout.js
@@ -0,0 +1,25 @@
+﻿var actionApp = angular.module('LayoutApp', ['commonApp']);
+
+app.directive("scroll", function ($window) {
+    return function (scope, element, attrs) {
+
+        angular.element($window).bind("scroll", function () {
+            console.log(this.pageYOffset);
+
+            if (this.pageYOffset < 10) {
+                scope.test = "animated fadeInDown";
+            }
+            else if ((this.pageYOffset > 20) || (this.pageYOffset < 30)) {
+                scope.test = "animated fadeOutUp";
+            }
+            if (this.pageYOffset >= 100) {
+                scope.boolChangeClass = true;
+                console.log('Scrolled below header.');
+            } else {
+                scope.boolChangeClass = false;
+                console.log('Header is in view.');
+            }
+            scope.$apply();
+        });
+    };
+});
